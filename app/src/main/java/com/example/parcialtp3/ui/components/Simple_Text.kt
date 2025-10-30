2ea51b5183f32327c0565997847ea42eb63ad313
Santiago <santiago.troiano13@gmail.com>
2025-10-30 00:11:35 -0300

    Agregado de componentes Simple_Text y Title_Text

---
+++ app/src/main/java/com/example/parcialtp3/ui/components/Simple_Text.kt
@@ -0,0 +1,41 @@
+package com.example.parcialtp3.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.example.parcialtp3.R
+
+private val PoppinsFamily = FontFamily(
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
+@Composable
+fun SimpleText(
+    text: String,
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit = 14.sp,
+    fontWeight: FontWeight = FontWeight.Normal,
+    color: Color = Color.Black,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontFamily = PoppinsFamily,
+        fontWeight = fontWeight,
+        textAlign = textAlign
+    )
+}
