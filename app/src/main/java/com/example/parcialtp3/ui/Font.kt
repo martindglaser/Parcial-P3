5c6be16ddd4cd0734551ae40dba5c4758dcbc013
martindglaser <65476570+martindglaser@users.noreply.github.com>
2025-10-30 15:45:07 -0300

    avance con la cabecera

---
+++ app/src/main/java/com/example/parcialtp3/ui/Font.kt
@@ -0,0 +1,11 @@
+package com.example.parcialtp3.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.parcialtp3.R
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
