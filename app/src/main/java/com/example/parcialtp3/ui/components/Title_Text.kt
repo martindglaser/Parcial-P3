d55f11b33bb8530758dbacb6f029bf3ed48b2e2c
Valentino Sara <valentino@amv.travel>
2025-10-30 22:47:16 -0300

    Cambio en el color por default

--- app/src/main/java/com/example/parcialtp3/ui/components/Title_Text.kt
+++ app/src/main/java/com/example/parcialtp3/ui/components/Title_Text.kt
@@ -4,13 +4,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.TextUnit
-import com.example.parcialtp3.R
 
 @Composable
 fun TitleText(
