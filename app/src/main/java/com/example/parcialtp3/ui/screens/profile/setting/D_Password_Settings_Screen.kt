75acc382a220999f22159c999879d19a023cbf28
Luciano Amato <luciano2697@outlook.com.ar>
2025-11-02 20:15:38 -0300

    Profile Settings Screens (#16)
    * Done: profile/settings/A,B,C,D,E,F-modal- | Added: icon_toggles, ConfirmDeleteDialog_modal
    
    * added .idea/ to .gitignore

--- app/src/main/java/com/example/parcialtp3/ui/screens/profile/setting/D_Password_Settings_Screen.kt
+++ app/src/main/java/com/example/parcialtp3/ui/screens/profile/setting/D_Password_Settings_Screen.kt
@@ -1,2 +1,20 @@
 package com.example.parcialtp3.ui.screens.profile.setting
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.parcialtp3.R
+import com.example.parcialtp3.ui.components.SuccessScreen
+
+@Composable
+fun D_Password_Settings_Screen() {
+    SuccessScreen(
+        message = "Password Has Been\nChanged Successfully",
+        iconRes = R.drawable.icon_check_progress1
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true, name = "Password changed")
+@Composable
+private fun PreviewFingerprintEliminate() {
+    D_Password_Settings_Screen()
+}
