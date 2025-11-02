b084c3b40bfcb52fd733da7f8392ba8369b93b45
Luciano Amato <luciano2697@outlook.com.ar>
2025-11-02 03:04:23 -0300

    Updated: E_Fingerprint_Screen

--- app/src/main/java/com/example/parcialtp3/ui/screens/profile/security/E_Fingerprint_Screen.kt
+++ app/src/main/java/com/example/parcialtp3/ui/screens/profile/security/E_Fingerprint_Screen.kt
@@ -1,21 +1,16 @@
 package com.example.parcialtp3.ui.screens.profile.security
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.parcialtp3.R
 import com.example.parcialtp3.ui.CaribbeanGreen
 import com.example.parcialtp3.ui.LightGreen
 import com.example.parcialtp3.ui.Void
