972cbb87fe33a88f57d4664e5f986d5d52468245
Luciano Amato <luciano2697@outlook.com.ar>
2025-11-02 02:37:09 -0300

    Added: SuccessScreen Component | updated with component: C_Change_Pin_Screen & F_Fingerprint_Screen

--- app/src/main/java/com/example/parcialtp3/ui/screens/profile/security/C_Change_Pin_Screen.kt
+++ app/src/main/java/com/example/parcialtp3/ui/screens/profile/security/C_Change_Pin_Screen.kt
@@ -1,51 +1,16 @@
 package com.example.parcialtp3.ui.screens.profile.security
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.parcialtp3.R
-import com.example.parcialtp3.ui.CaribbeanGreen
-import com.example.parcialtp3.ui.LightGreen
+import com.example.parcialtp3.ui.components.SuccessScreen
 
 @Composable
-fun C_Change_Pin_Success_Screen(
-    message: String = "Pin Has Been\nChanged Successfully",
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(CaribbeanGreen)
-            .padding(WindowInsets.systemBars.asPaddingValues()),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Icon(
-                painter = painterResource(R.drawable.icon_check_progress1),
-                contentDescription = null,
-                modifier = Modifier.size(140.dp),
-                tint = Color.Unspecified
-            )
-            Spacer(Modifier.height(24.dp))
-            Text(
-                text = message,
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold,
-                color = LightGreen
-            )
-        }
-    }
+fun C_Change_Pin_Success_Screen() {
+    SuccessScreen(
+        message = "Pin Has Been\nChanged Successfully",
+        iconRes = R.drawable.icon_check_progress1
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true, name = "Success")
